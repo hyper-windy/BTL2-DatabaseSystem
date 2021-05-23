7a805bec1d6e1cd6ac6866b7266b09fea4b44c7c
toanvo4121 <58241275+toanvo4121@users.noreply.github.com>
2021-05-23 20:11:32 +0700

    Add files via upload

---
+++ table_toan.sql
@@ -0,0 +1,35 @@
+CREATE TABLE TUYENTAU_XE (
+	ma_tuyen 			CHAR(4) 							PRIMARY KEY,
+    CONSTRAINT 			_name1
+		CHECK	(SUBSTRING(ma_tuyen,1,1) = 'B' or SUBSTRING(ma_tuyen,1,1) = 'T'),
+	CONSTRAINT 			_name2
+		CHECK	(CAST(SUBSTRING(ma_tuyen,2,3) as SIGNED) >= 0 and CAST(SUBSTRING(ma_tuyen,2,3) as SIGNED) <= 999)
+);
+
+CREATE TABLE TUYENXEBUS (
+    _no INT AUTO_INCREMENT PRIMARY KEY,
+    ma_tuyen_tau_xe CHAR(4)
+);
+
+CREATE TABLE TUYENTAUDIEN (
+	ma_tuyen_tau 		CHAR 								PRIMARY KEY,
+    ten_tuyen_tau 		VARCHAR(25) 	UNIQUE NOT NULL,
+	ma_tuyen_tau_xe 	CHAR(4)
+);
+
+CREATE TABLE CHUYENTAUXE (
+	ma_tuyen 			CHAR(4),
+    stt 				INT 			UNSIGNED,
+    PRIMARY KEY (ma_tuyen, stt)
+);
+
+
+ALTER TABLE TUYENXEBUS
+	ADD CONSTRAINT fkey_1 
+		FOREIGN KEY TUYENXEBUS (ma_tuyen_tau_xe) REFERENCES TUYENTAU_XE (ma_tuyen);
+ALTER TABLE TUYENTAUDIEN
+	ADD CONSTRAINT fkey_2
+		FOREIGN KEY TUYENTAUDIEN (ma_tuyen_tau_xe) REFERENCES TUYENTAU_XE (ma_tuyen);
+ALTER TABLE CHUYENTAUXE
+	ADD CONSTRAINT fkey_3
+		FOREIGN KEY CHUYENTAUXE (ma_tuyen) REFERENCES TUYENTAU_XE (ma_tuyen);
